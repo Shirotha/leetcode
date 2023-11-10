40617588a5ea50a6605500ec32876e56b9232951
Shirotha <zimmermann-sven@online.de>
2023-11-10 13:09:45 +0100

    add_binary solved

---
+++ src/problems/add_binary.rs
@@ -0,0 +1,83 @@
+/*
+    Given two binary strings a and b, return their sum as a binary string.
+
+    Constraints:
+
+    1 <= a.length, b.length <= 10^4
+    a and b consist only of '0' or '1' characters.
+    Each string does not contain leading zeros except for the zero itself.
+ */
+
+pub struct Solution;
+
+use std::{iter::repeat, collections::VecDeque, mem::ManuallyDrop};
+
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        const ZERO: u8 = b'0';
+        let (mut n, a, b) = if a.len() >= b.len() {
+            (a.len(), a, b)
+        } else { (b.len(), b, a) }; 
+        let mut digits = ManuallyDrop::new(VecDeque::with_capacity(n + 1));
+        let mut carry = 0;
+        for (&a, &b) in a.as_bytes().iter().rev()
+            .zip(b.as_bytes().iter().rev().chain(repeat(&ZERO))) 
+        {
+            let sum = (a - ZERO) + (b - ZERO) + carry;
+            digits.push_front(ZERO + (sum & 1));
+            carry = (sum & 2) >> 1; n -= 1;
+        }
+        if carry != 0 { digits.push_front(ZERO + 1); }
+        unsafe {
+            let buf = digits.make_contiguous().as_mut_ptr();
+            String::from_raw_parts(buf, digits.len(), digits.capacity())
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    /*
+        Input: a = "11", b = "1"
+        Output: "100"
+     */
+    #[test]
+    fn example1() {
+        let a = "11".to_string();
+        let b =  "1".to_string();
+
+        let s = Solution::add_binary(a, b);
+
+        assert_eq!(&s, &"100");
+    }
+
+    /*
+        Input: a = "1010", b = "1011"
+        Output: "10101"
+     */
+    #[test]
+    fn example2() {
+        let a = "1010".to_string();
+        let b = "1011".to_string();
+
+        let s = Solution::add_binary(a, b);
+
+        assert_eq!(&s, &"10101");
+    }
+
+    /*
+        Input: a = "100", b = "110010"
+        Output: "110110"
+     */
+    #[test]
+    fn example3() {
+        let a =    "100".to_string();
+        let b = "110010".to_string();
+
+        let s = Solution::add_binary(a, b);
+
+        assert_eq!(&s, &"110110");
+    }
+}
--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -117,3 +117,4 @@ mod find_minimum_in_rotated_sorted_array;
 mod median_of_two_sorted_arrays;
 mod kth_largest_element_in_an_array;
 mod find_median_from_data_stream;
+mod add_binary;
